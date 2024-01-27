768595f41c0596af80e8468d7fdfe1798674359a
unknown <samuelwmorey1@gmail.com>
2024-01-27 08:49:01 -0800

    Adding temp conds for player select

--- scripts/SplitScreenManager.cs
+++ scripts/SplitScreenManager.cs
@@ -4,14 +4,14 @@ using System.Collections.Generic;
 
 public partial class SplitScreenManager : Node
 {
-	private int _numberOfPlayers = 4;
+	private int _numberOfPlayers = 3;
 	struct Player
 	{
-		public Viewport Viewport;
+		public SubViewport Viewport;
 		public Camera2D Camera2D;
 		public Node PlayerNode;
 
-		public Player(Viewport viewport, Camera2D camera2D, Node playerNode)
+		public Player(SubViewport viewport, Camera2D camera2D, Node playerNode)
 		{
 			Viewport = viewport;
 			Camera2D = camera2D;
@@ -24,16 +24,26 @@ public partial class SplitScreenManager : Node
 	public override void _Ready()
 	{
 		_playersInfo = new List<Player>();
-		for (int i = 0; i < _numberOfPlayers; i++)
+		for (int i = 0; i < 4; i++)
 		{
+			
 			Player tempPlayer = new Player(
-				GetNode<Viewport>($"GridContainer/SubViewportContainer{i.ToString()}/SubViewport"),
+				GetNode<SubViewport>($"GridContainer/SubViewportContainer{i.ToString()}/SubViewport"),
 				GetNode<Camera2D>($"GridContainer/SubViewportContainer{i.ToString()}/SubViewport/Camera2D"),
 				GetNode<Node>($"GridContainer/SubViewportContainer0/SubViewport/Game/Player{i.ToString()}"));
 			GD.Print(tempPlayer.PlayerNode);
 			if (i > 0)
 			{
 				tempPlayer.Viewport.World2D = _playersInfo[0].Viewport.World2D;
+			}
+
+			if (i > _numberOfPlayers)
+			{
+				
+				tempPlayer.PlayerNode.QueueFree();
+				tempPlayer.Viewport.QueueFree();
+				tempPlayer.Camera2D.QueueFree();
+				continue;
 			}
 			_playersInfo.Add(tempPlayer);
 		}
@@ -43,6 +53,20 @@ public partial class SplitScreenManager : Node
 			RemoteTransform2D remoteTransform2D = new RemoteTransform2D();
 			remoteTransform2D.RemotePath = playerInfo.Camera2D.GetPath();
 			playerInfo.PlayerNode.AddChild(remoteTransform2D);
+		}
+
+		switch (_numberOfPlayers)
+		{
+			case 1: //Just load level
+				break;
+			case 2:
+				foreach (var playerInfo in _playersInfo)
+				{
+					playerInfo.Viewport.Size = new Vector2I(960, 1080);
+				}
+				break;
+			case 3: //Design discussion about what we should do 
+				break;
 		}
 	}		
 
